b07b59fac0dd1018a195924497e2007724d83b1b
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-08-16 01:28:19 -0700

    models/index.js updated, relationships between models defined, API product-routes findAll and findOne

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,56 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll(
+    {
+      include: [
+        {
+          model: Category,
+          attributes: ['category_name']
+        },
+        {
+          model: Tag,
+          attributes: ['tag_name'],
+          through: ProductTag,
+          as: 'product_tag'
+        }
+      ]
+    }
+  )
+  .then(productData => res.json(productData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne(
+    {
+      where: {
+        id: req.parem.id
+      },
+      include: [
+        {
+          model: Category
+        },
+        {
+          model: Tag,
+          attributes: ['tag_name'],
+          through: ProductTag,
+          as: 'product_tag'
+        }
+      ]
+    }
+  )
+  .then(productData => res.json(productData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // create new product
